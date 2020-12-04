01b493251f8a8e0305206142893906dd1f139f11
florisvdh <floris.vanderhaeghe@inbo.be>
2020-12-04 11:05:31 +0100

    Elaborate script *
    - add columns grts_rank, haspengouw, grts_rank_haspengouw
    - improve maps
    - create points object (1 haspengouw unit has 2 points)
    - write more geospatial objects

--- sortering_eenheden7220_vlaanderen.R
+++ sortering_eenheden7220_vlaanderen.R
@@ -27,6 +27,13 @@ border_cases <-
   select(unit_id, grts_address) %>%
   st_drop_geometry()
 
+# bounding box voor de 7220 eenheden in Haspengouw:
+bbox_zlimb <- st_bbox(c(xmin = 2.05e5,
+                        xmax = 2.4e5,
+                        ymin = 15.5e4,
+                        ymax = 17e4),
+                      crs = st_crs(31370))
+
 units_7220_ranked <-
   units_7220_ranked %>%
   # opvullen ontbrekende GRTS rank:
@@ -36,35 +43,95 @@ units_7220_ranked <-
                               pull(grts_address),
                             grts_address)) %>%
   # sorteren volgens GRTS rank:
-  arrange(grts_address)
+  arrange(grts_address) %>%
+  mutate(grts_rank = rank(grts_address),
+         .after = grts_address) %>%
+  st_join(bbox_zlimb %>%
+            st_as_sfc %>%
+            st_as_sf(haspengouw = TRUE)) %>%
+    mutate(grts_rank_haspengouw = dense_rank(grts_address * haspengouw),
+           haspengouw = !is.na(haspengouw)) %>%
+    relocate(haspengouw, grts_rank_haspengouw,
+           .after = grts_rank)
 
-units_7220_ranked %>% st_write("units_7220_ranked.gpkg",
+
+# Overeenkomstige 7220-punten (= subniveau van 'units') in habitatsprings:
+
+points_7220_ranked <-
+  read_habitatsprings(filter_hab = TRUE) %>%
+  inner_join(units_7220_ranked %>%
+               st_drop_geometry %>%
+               select(unit_id,
+                      matches("grts|hasp")),
+             by = "unit_id") %>%
+  relocate(matches("grts|hasp"), .after = unit_id) %>%
+  arrange(grts_address, point_id)
+
+points_7220_ranked %>%
+  filter(haspengouw)
+
+# Wegschrijven van de resultaten:
+
+if (!dir.exists("output")) dir.create("output")
+
+units_7220_ranked %>% st_write("output/type7220_ranked.gpkg",
+                               layer = "units_7220_ranked",
                                delete_dsn = TRUE)
-units_7220_ranked %>% st_write("units_7220_ranked.geojson",
+points_7220_ranked %>% st_write("output/type7220_ranked.gpkg",
+                               layer = "points_7220_ranked",
+                               delete_dsn = TRUE)
+units_7220_ranked %>% st_write("output/units_7220_ranked.shp",
+                               delete_dsn = TRUE)
+points_7220_ranked %>% st_write("output/points_7220_ranked.shp",
+                               delete_dsn = TRUE)
+  # Laatste versie van gpkg & shp: see <gdrive link>.
+  # Shapefile is een formaat met afgekapte veldnamen.
+
+  # GeoJSON files voor bewaring in de git repo:
+
+units_7220_ranked %>% st_write("output/units_7220_ranked.geojson",
                                layer_options = "RFC7946=YES",
                                delete_dsn = TRUE)
-units_7220_ranked %>% st_write("units_7220_ranked.shp",
-                               delete_dsn = TRUE)
-
-# op kaart:
-
-mapview(units_7220_ranked, zcol = "grts_address")
-
-ggplot(units_7220_ranked) +
-  geom_sf(data = flanders, fill = NA) +
-  geom_sf(aes(colour = grts_address)) +
-  coord_sf(datum = 31370) +
-  theme_bw() +
-  lims(x = c(2.05e5, 2.4e5), y = c(15.5e4, 17e4))
-
-bbox_zlimb <- st_bbox(c(xmin = 2.05e5,
-                        xmax = 2.4e5,
-                        ymin = 15.5e4,
-                        ymax = 17e4),
-                      crs = st_crs(31370))
 
 units_7220_ranked %>%
-  st_intersection(st_as_sfc(bbox_zlimb)) %>%
-  select(1:6)
+  filter(haspengouw) %>%
+  st_write("output/units_7220_haspengouw_ranked.geojson",
+           layer_options = "RFC7946=YES",
+           delete_dsn = TRUE)
+
+points_7220_ranked %>% st_write("output/points_7220_ranked.geojson",
+                                layer_options = "RFC7946=YES",
+                                delete_dsn = TRUE)
+
+points_7220_ranked %>%
+  filter(haspengouw) %>%
+  st_write("output/points_7220_haspengouw_ranked.geojson",
+           layer_options = "RFC7946=YES",
+           delete_dsn = TRUE)
+
+# Een GeoJSON inlezen en transformeren naar CRS 'Belge 1972 / Belgian Lambert 72':
+read_sf("output/units_7220_haspengouw_ranked.geojson") %>%
+  st_transform(31370)
+
+# Units op kaart:
+
+mapview(units_7220_ranked %>% filter(haspengouw),
+        layer.name = "units_7220",
+        zcol = "grts_rank_haspengouw",
+        alpha.regions = 1,
+        label = "grts_rank_haspengouw")
+
+units_7220_ranked %>%
+  filter(haspengouw) %>%
+  mutate(grts_address = factor(grts_address)) %>%
+  ggplot() +
+  geom_sf(data = flanders, fill = NA) +
+  geom_sf(aes(fill = grts_address),
+          size = 3,
+          shape = 21) +
+  coord_sf(datum = 31370) +
+  theme_bw() +
+  lims(x = c(bbox_zlimb$xmin, bbox_zlimb$xmax),
+       y = c(bbox_zlimb$ymin, bbox_zlimb$ymax))
 
 
